b2c35985d923a1c9c8edd356670429a8115db8c8
Kevin St.Onge <kevinstonge@gmail.com>
2020-12-15 11:52:16 -0500

    basic setup of client and server apps

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const server = express();
+server.use(express.json());
+const helmet = require('helmet');
+server.use(helmet());
+const cors = require('cors');
+server.use(cors());
+
+const path = require("path");
+
+server.use('/api', (req, res) => { //todo: set up api router
+    res.json({ message:"test"})
+})
+
+server.use(express.static('./client/build/'));
+
+server.get("*", (req, res) => { 
+ res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+
+
+module.exports = server;
