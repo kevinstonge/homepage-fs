3d1e8ae82a45f05f6b14dc16456880496eb91f8c
Kevin St.Onge <kevinstonge@gmail.com>
2021-03-09 09:27:04 -0500

    csp

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const path = require("path");
 server.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
-    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+    "default-src 'self' http://www.kevinstonge.com http://kevinstonge.com;"
   );
   next();
 });
