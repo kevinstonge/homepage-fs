9c1811bf9c5087041193fd64a2a0e366e8e23a53
Kevin StOnge <kevinstonge@gmail.com>
2021-03-12 16:00:29 -0500

    idk

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,8 +1,18 @@
 function Skills() {
   const skills = [
+    { name: "HTML5", proficiency: 5 },
+    { name: "CSS3", proficiency: 4 },
     { name: "Javascript", proficiency: 4 },
     { name: "ReactJS", proficiency: 4 },
   ];
+  const projects = [
+    {
+      name: "color palette generator",
+      url: "http://colors.kevinstonge.com",
+      skills: [],
+    }, //could do this with a database ... not sure if worth the effort
+  ];
+
   return (
     <>
       <h2>Skills</h2>
