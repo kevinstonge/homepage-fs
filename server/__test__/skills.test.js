7d4e00f84ad3cff7d942321d69a9cad1bef74b48
Kevin St.Onge <kevinstonge@gmail.com>
2021-04-09 13:35:22 -0400

    attempting to add file upload capability

--- server/__test__/skills.test.js
+++ server/__test__/skills.test.js
@@ -1,11 +1,17 @@
 const request = require("supertest");
 const server = require("../server.js");
 const db = require("../data/dbConfig.js");
-const { default: expectCt } = require("helmet/dist/middlewares/expect-ct");
+const path = require('path');
 beforeAll(async () => {
   await db("projects-skills").truncate();
   await db("skills").truncate();
   await db("projects").truncate();
+});
+describe("POST requests to /api/portfolio/skills with bad input", () => {
+    it("should respond with 400 if incomplete data is provided", async () => {
+      const result = await request(server).post("/api/portfolio/skills").send({ "short_name": "js" });
+        expect(result.status).toBe(400);
+    });
 });
 describe("GET requests to /api/portfolio/skills", () => {
   it("should respond with full list of skills", async () => {
@@ -18,15 +24,27 @@ describe("POST requests to /api/portfolio/skills", () => {
     const result = await request(server)
       .post("/api/portfolio/skills")
       .send({
-        skill: {
           long_name: "JavaScript (ECMA Script 5)",
           short_name: "js",
           proficiency: 3,
-        },
       });
-    expect(result.body.newSkill.length).toBe(1);
+    expect(result.body.addedSkill.length).toBe(1);
   });
 });
+describe("POST requests to /api/portfolio/skills with image attachment", () => {
+  it("should respond with 201", async () => {
+    request(server)
+      .post('/api/portfolio/skills')
+      .set('Accept', /application\/json/)
+      .type("form")
+      .field('long_name', "HTML 5")
+      .field('short_name', "html")
+      .field('proficiency', 3)
+      .attach('logo', path.join(__dirname, "../images/test.png"))
+      .expect(201)
+      .end(function (err, res) { console.log(err); });;
+  })
+})
 describe("GET requests to /api/portfolio/skills", () => {
   it("should respond with full list of skills", async () => {
     const result = await request(server).get("/api/portfolio/skills");
@@ -34,9 +52,4 @@ describe("GET requests to /api/portfolio/skills", () => {
     expect(result.body.skills[0].short_name).toBe("js");
   });
 });
-// describe("POST requests to /api/portfolio/skills with bad input", () => {
-//     it("should respond with 400 if incomplete data is provided", async () => {
-//         const result = await request(server).post("/api/portfolio/skills").send({ "long_name": "JavaScript (ECMA Script 5)" });
-//         expect(result.status).toBe(400);
-//     });
-// });
+
--- server/api/portfolio/skillsRouter.js
+++ server/api/portfolio/skillsRouter.js
@@ -1,6 +1,14 @@
 const Skills = require("./skillsModel");
 const router = require("express").Router();
-
+const multer = require('multer');
+const path = require("path");
+const storage = multer.diskStorage({
+  destination: "/images/",
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + Date.now() + path.extname(file.originalname))
+  }
+})
+//200 OK, 201 CREATED, 400 BAD REQUEST, 401 UNAUTHORIZED, 500 INTERNAL SERVER ERROR
 // router.use('/:id', validation middleware)
 router.get("/", async (req, res) => {
   try {
@@ -14,17 +22,53 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   //long_name (required), short_name, logo, proficiency
   try {
-    if (req.body.skill.long_name && req.body.skill.long_name.length > 0) {
-      const newSkill = await Skills.addSkill(req.body.skill);
-      if (newSkill) {
-        res.status(200).json({ newSkill });
+    console.log(req.body);
+    let logoPath = "";
+    if (req.body.logo) {
+      const upload = multer({
+        storage: storage,
+        fileFilter: () => {
+          if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+            req.fileValidationError = 'Only image files are allowed!';
+            return cb(new Error('Only image files are allowed!'), false);
+          }
+          cb(null, true);
+        },
+      }).single('logo');
+      upload(req, res, (err) => {
+        if (req.fileValidationError) {
+          return res.send(req.fileValidationError);
+        }
+        else if (!req.body.logo) {
+          return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+          return res.send(err);
+        }
+        else if (err) {
+          return res.send(err);
+        }
+        logoPath = req.body.logo.path;
+      });
+    }
+    if (req.body.long_name && req.body.long_name.length > 0) {
+      const newSkill = {
+        long_name: req.body.long_name,
+        short_name: req.body.short_name || "",
+        proficiency: req.body.proficiency || 0,
+        logo: req.body.logo?.path || ""
+      }
+      const addedSkill = await Skills.addSkill(newSkill);
+      if (addedSkill) {
+        res.status(201).json({ addedSkill });
       }
     } else {
-      res.status(500).json({ message: "error1" });
+      res.status(400).json({ message: "skills must include a long_name value" });
     }
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "error2" });
+    res.status(500).json({ message: "server failed to create the skill" });
+    throw err;
   }
 });
 
