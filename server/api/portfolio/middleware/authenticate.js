57ae3f6dee759a40ac4e749add65af2f0c9679f3
Kevin StOnge <kevinstonge@gmail.com>
2021-04-28 08:55:51 -0400

    authentication via cookie or token

--- server/api/portfolio/middleware/authenticate.js
+++ server/api/portfolio/middleware/authenticate.js
@@ -1,14 +1,22 @@
 const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
-  if (
-    req.cookies &&
-    req.cookies.auth &&
-    jwt.verify(req.cookies.auth, process.env.JWT_SECRET)
-  ) {
-    if (next !== undefined) { next() }
-    else { return true }
+  let token = "invalid";
+  if (req.cookies?.auth) {
+    token = req.cookies.auth;
+  } else if (req.headers?.authorization) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+  if (jwt.verify(token, process.env.JWT_SECRET)) {
+    if (next !== undefined) {
+      next();
+    } else {
+      return true;
+    }
   } else {
-    if (next !== undefined) { res.status(401).json({ message: "unauthorized" }) }
-    else { return false }
+    if (next !== undefined) {
+      res.status(401).json({ message: "unauthorized" });
+    } else {
+      return false;
+    }
   }
 };
