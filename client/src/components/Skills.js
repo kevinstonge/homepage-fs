94c4723b60029e1b421b8e4d384b3321a1df39c4
Kevin St.Onge <kevinstonge@gmail.com>
2021-05-08 13:15:37 -0400

    working on client skills page

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,24 +1,58 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "../style/Skills.scss";
 function Skills() {
-  const skills = [
-    { name: "HTML5", proficiency: 5 },
-    { name: "CSS3", proficiency: 4 },
-    { name: "Javascript", proficiency: 4 },
-    { name: "ReactJS", proficiency: 4 },
-  ];
-  const projects = [
-    {
-      name: "color palette generator",
-      url: "http://colors.kevinstonge.com",
-      skills: [],
-    }, //could do this with a database ... not sure if worth the effort
-  ];
-
+  const proficiency = {
+    1: "beginner",
+    2: "intermediate",
+    3: "advanced",
+    4: "expert"
+  }
+  const [projects, setProjects] = useState([]);
+  const [skills, setSkills] = useState([]);
+  const [status, setStatus] = useState("... fetching data from server ...");
+  useEffect(()=>{
+    axios.get(`https://www.kevinstonge.com/api/portfolio/projects`).then(r=>{
+      if (r.status === 200) {
+        setProjects(r.data.projects);
+        setSkills(r.data.skills);
+        setStatus(null);
+      }  
+      else {
+        console.log('error');
+        setStatus('error getting data from server')
+      }
+    })
+  },[])
   return (
     <>
-      <h2>Skills</h2>
-      {skills.map((skill, id) => (
-        <p key={`${skill.name}-${id}`}>{skill.name}</p>
-      ))}
+      {skills.length > 0 &&
+        <table>
+        <caption>Skills</caption>
+        <thead><tr><th>skill</th><th>proficiency</th></tr></thead>
+        <tbody>
+        {/* long_name, short_name, logo, proficiency */}
+        {skills.map((skill, id) => (
+          <tr key={`skill${skill.id}`}>
+            <td>
+              <img src={`https://www.kevinstonge.com/images/${skill.logo}`}/>
+              <p>{skill.long_name}</p>
+            </td>
+            <td>{proficiency[skill.proficiency]}</td>
+          </tr>
+        ))}
+        </tbody>
+        </table>
+      }
+      {projects.length > 0 && 
+        <>
+        <h2>Projects</h2>
+        {projects.length > 0 && projects.map((project, id) => (
+          <p key={`${project.title}-${id}`}>{project.title}</p>
+        ))}
+        </>
+     }
+      {status !== null && <p>{status}</p>}
     </>
   );
 }
