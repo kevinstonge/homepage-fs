1d31db53db7a5c3b652d442899a6d76f328b2e83
Kevin StOnge <kevinstonge@gmail.com>
2021-05-20 19:04:08 -0400

    trying flexbox for skills layout

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -6,52 +6,85 @@ function Skills() {
     1: "beginner",
     2: "intermediate",
     3: "advanced",
-    4: "expert"
-  }
+    4: "expert",
+  };
   const [projects, setProjects] = useState([]);
   const [skills, setSkills] = useState([]);
   const [status, setStatus] = useState("... fetching data from server ...");
-  useEffect(()=>{
-    axios.get(`https://www.kevinstonge.com/api/portfolio/projects`).then(r=>{
-      if (r.status === 200) {
-        setProjects(r.data.projects);
-        setSkills(r.data.skills);
-        setStatus(null);
-      }  
-      else {
-        console.log('error');
-        setStatus('error getting data from server')
-      }
-    })
-  },[])
+  useEffect(() => {
+    axios
+      .get(`https://www.kevinstonge.com/api/portfolio/projects`)
+      .then((r) => {
+        if (r.status === 200) {
+          setProjects(r.data.projects);
+          setSkills(r.data.skills);
+          setStatus(null);
+        } else {
+          console.log("error");
+          setStatus("error getting data from server");
+        }
+      });
+  }, []);
   return (
     <>
-      {skills.length > 0 &&
-        <table>
-        <caption>Skills</caption>
-        <thead><tr><th>skill</th><th>proficiency</th></tr></thead>
-        <tbody>
-        {/* long_name, short_name, logo, proficiency */}
-        {skills.map((skill, id) => (
-          <tr key={`skill${skill.id}`}>
-            <td>
-              <img src={`https://www.kevinstonge.com/images/${skill.logo}`}/>
-              <p>{skill.long_name}</p>
-            </td>
-            <td>{proficiency[skill.proficiency]}</td>
-          </tr>
-        ))}
-        </tbody>
-        </table>
-      }
-      {projects.length > 0 && 
+      {skills.length > 0 && (
         <>
-        <h2>Projects</h2>
-        {projects.length > 0 && projects.map((project, id) => (
-          <p key={`${project.title}-${id}`}>{project.title}</p>
-        ))}
+          <h2>My technical skills</h2>
+          <div className="skills-table">
+            <div className="row">
+              <p>skill</p>
+              <p>proficiency</p>
+            </div>
+            {skills.map((skill) => (
+              <div className="row" key={`${skill.long_name}`}>
+                <div className="img-name">
+                  <img
+                    src={`https://www.kevinstonge.com/images/${skill.logo}`}
+                    alt={`${skill.long_name}`}
+                  />
+                  <p>{skill.long_name}</p>
+                </div>
+                <p>{proficiency[skill.proficiency]}</p>
+              </div>
+            ))}
+          </div>
         </>
-     }
+        // <table>
+        //   <caption>Skills</caption>
+        //   <thead>
+        //     <tr>
+        //       <th colSpan="2">skill</th>
+        //       <th style={{ width: "100%" }}>proficiency</th>
+        //     </tr>
+        //   </thead>
+        //   <tbody>
+        //     {/* long_name, short_name, logo, proficiency */}
+        //     {skills.map((skill) => (
+        //       <tr key={`skill${skill.id}`}>
+        //         <td className="img-container">
+        //           <img
+        //             src={`https://www.kevinstonge.com/images/${skill.logo}`}
+        //             alt={`${skill.long_name}`}
+        //           />
+        //         </td>
+        //         <td>
+        //           <p>{skill.long_name}</p>
+        //         </td>
+        //         <td>{proficiency[skill.proficiency]}</td>
+        //       </tr>
+        //     ))}
+        //   </tbody>
+        // </table>
+      )}
+      {projects.length > 0 && (
+        <>
+          <h2>Projects</h2>
+          {projects.length > 0 &&
+            projects.map((project, id) => (
+              <p key={`${project.title}-${id}`}>{project.title}</p>
+            ))}
+        </>
+      )}
       {status !== null && <p>{status}</p>}
     </>
   );
