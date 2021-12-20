b0037db2c4d3557de54b7f5d81ba3ea588fae4cf
Kevin St.Onge <kevinstonge@gmail.com>
2021-12-20 09:44:09 -0500

    created about page

---
+++ client/src/components/About.js
@@ -0,0 +1,7 @@
+function About() {
+  return (<>
+    <h2>About Kevin St.Onge</h2>
+    <p>I've always loved using and learning about technology.</p>
+  </>)
+}
+export default About;
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "../style/App.scss";
 import Header from "./Header.js";
 import Skills from "./Skills.js";
+import About from "./About.js";
 import Contact from "./Contact.js";
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
               <Skills />
             </Route>
             <Route path="/about">
-              <p>about</p>
+              <About />
             </Route>
             <Route path="/contact">
               <Contact />
